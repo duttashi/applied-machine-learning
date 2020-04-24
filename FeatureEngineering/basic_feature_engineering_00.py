c626d9b1ba312a938370be8ab0de297e286b9924
ashish <ashish.dutt8@gmail.com>
2020-04-24 11:13:54 +0800

    modified script
    - added code for correlation detection

--- FeatureEngineering/basic_feature_engineering_00.py
+++ FeatureEngineering/basic_feature_engineering_00.py
@@ -12,6 +12,7 @@ import pandas as pd, numpy as np, matplotlib.pyplot as plt, seaborn as sns
 from scipy import stats
 from scipy.stats import norm, skew #for some statistics
 
+
 color = sns.color_palette()
 sns.set_style('darkgrid')
 import warnings
@@ -105,3 +106,24 @@ sns.barplot(x=train_data_na.index, y=train_data_na)
 plt.xlabel('Features', fontsize=15)
 plt.ylabel('Percent of missing values', fontsize=15)
 plt.title('Percent missing data by feature', fontsize=15)
+
+# Data correlation
+# plot a correlation map to see how the features are related to variable SalePrice
+corr_mat=train_data.corr()
+plt.subplots(figsize=(12,9))
+sns.heatmap(corr_mat, vmax=0.9, square=True)
+
+# Missing data imputation
+train_data["PoolQC"]=train_data["PoolQC"].fillna("None")
+train_data['MiscFeature']=train_data["MiscFeature"].fillna("None")
+train_data["Alley"]=train_data["Alley"].fillna("None")
+train_data["Fence"]=train_data["Fence"].fillna("None")
+train_data["FireplaceQu"]=train_data["FireplaceQu"].fillna("None")
+train_data["LotFrontage"]=train_data["LotFrontage"].fillna("None")
+
+train_data_na=(train_data.isnull().sum()/len(train_data))*100
+train_data_na=train_data_na.drop(train_data_na[train_data_na == 0].index).sort_values(ascending=False)[:30]
+train_data_missing=pd.DataFrame({'Missing Ratio': train_data_na})
+print("\n",train_data_missing)
+
+
