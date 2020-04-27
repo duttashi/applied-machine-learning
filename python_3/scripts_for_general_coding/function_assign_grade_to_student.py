8207875b7b5df22c20ddab60dd14b5eba4046ac3
ashish <ashish.dutt8@gmail.com>
2020-04-28 07:49:37 +0800

    modified code
    - added functionality to accept a list of items as arguments to a function
    - added functionality to return a list of items from a function

--- python_3/scripts_for_general_coding/function_assign_grade_to_student.py
+++ python_3/scripts_for_general_coding/function_assign_grade_to_student.py
@@ -6,26 +6,40 @@ Created on Mon Apr 27 11:00:56 2020
 
 Use the logic for assigning grade to student defined in
 assign_grade_to_student.py script.
-Now, create a function for the same
+The idea is to create a function that will collect student
+exam related data from the user like roll number, test mark, exam mark etc.
+It will then pass this data as argument into another function.
+The other function will pull the data out of the arguments
+and use them for further analysis.
+
 
 """
+
+# declare a global list to hold the multiple variable passed by the function
+student_info_list = []
 
 
 def get_student_details():
     stud_num = input("Enter the student number: ")
     stud_tutorial_mark = float(input("Enter student's tutorial mark:"))
     stud_test_mark = float(input("Enter student's test mark:"))
-    return stud_num, stud_test_mark, stud_tutorial_mark
+    stud_exam_mark = float(input("Enter student exam mark: "))
+    # add data to list
+    student_info_list.append(stud_num)
+    student_info_list.append(stud_tutorial_mark)
+    student_info_list.append(stud_test_mark)
+    student_info_list.append(stud_exam_mark)
+    # return a list of items
+    return student_info_list
 
 
-def calculate_grade(sno, test_mark, tut_mark):
-    stud_num = sno
-    stud_test_mark = test_mark
-    stud_tutorial_mark = tut_mark
+def calculate_grade(student_info_list):
+    stud_num = student_info_list[0]
+    stud_test_mark = student_info_list[1]
+    stud_tutorial_mark = student_info_list[2]
+    stud_exam_mark = student_info_list[3]
     if stud_tutorial_mark+stud_test_mark/2 < 40:
         grade = "Fail"
-    else:
-        stud_exam_mark = float(input("Enter student exam mark: "))
     final_mark = (stud_tutorial_mark+stud_test_mark+2*stud_exam_mark)/4
     if 80 <= final_mark <= 100:
         grade = "A"
@@ -42,12 +56,5 @@ def calculate_grade(sno, test_mark, tut_mark):
 
 # call the function
 get_stud_info = get_student_details()
-print(get_stud_info)
-snum = get_stud_info[0]
-testmarks = get_stud_info[1]
-tutmarks = get_stud_info[2]
-print(snum, testmarks, tutmarks)
-print(calculate_grade(snum, testmarks, tutmarks))
-# get_report_card = calculate_grade(snum, testmarks, tutmarks)
-# print("student %s's grade is %s." % (stud_num, grade))
-# print(get_report_card)
+snum, fmark, grade = calculate_grade(get_stud_info)
+print("student %s's final mark is %d and grade is %s." % (snum, fmark, grade))
