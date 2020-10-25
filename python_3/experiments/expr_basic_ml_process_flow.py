7001727dd2986a3609039c5742f313f56d9490cb
ashish <ashish.dutt8@gmail.com>
2020-10-25 13:31:12 +0800

    modified script
    - add new code

--- python_3/experiments/expr_basic_ml_process_flow.py
+++ python_3/experiments/expr_basic_ml_process_flow.py
@@ -1,28 +1,46 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct 23 17:06:09 2020
+Created on Sun Oct 25 08:12:59 2020
 
 @author: Ashish
 """
 
-from sklearn.datasets import load_iris
+# load the required libraries
+import pandas as pd
 
-iris = load_iris()
-print(type(iris))
-print(iris.feature_names)
-print(iris.target)
-print(iris.target_names) # 0-setosa, 1-versicolor, 2-virginica
+# load the required data
+df = pd.read_csv("../../data/adult.csv")
+# Exploratory data analysis
+##  look at data shape, number of cols, column type, missing vals etc
+print("Original data shape: ",df.shape)
+print(df.columns)
+print(df.head)
+# Observations: assign column names, remove irrelevant cols
+col_header = ['age','workclass','fnlwgt','education','education-num',
+              'marital-status','occupation','relationship','race','sex',
+              'capital-gain','capital-loss','hours-per-week','native-country','salary']
+# assign column headers
+df.columns = col_header
+print(df.columns)
 
-print(iris.data.shape)
-print(iris.target.shape)
+# remove irrelevant cols based on theri index number
+# NOTE: Pandas column indexing begins from ZERO and not from One.
+drop_cols = [2,4,10,11]
+df.drop(df.columns[drop_cols],axis=1,inplace=True)
+print("reduced data shape: ", df.shape)
+print("reduced data columns: ",df.columns)
 
-# store feature matrix in X
-# X is capitalised because it represents a matrix
-X = iris.data
-# store response data in y
-# y is lowercase because it represents a vector
-y = iris.target
+# check for missing values
+print("missing value count: ")
+print(df.isnull().sum()) # No missing values
 
-# scikit learn 4 step process
-# import the class you want to use
-import sklearn.neighbours import K
+# convert categorical cols to numerical format
+
+# 1. check for column data types
+print("Column data types: ",df.dtypes)
+# 2. Convert selected object data types to category data type
+cat_cols = ['workclass','education','marital-status','occupation','relationship','race','sex','native-country','salary']
+# 2. select all categorical columns using select.dtypes()
+df = df[cat_cols].astype('category')
+print("Revised column data types ", df.columns)
+# check for high correlated variables
