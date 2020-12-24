44ea2a4e3d2f57a8320dc9da40231da8bd1e904a
Ashish Dutt <ashish.dutt8@gmail.com>
2020-12-24 17:39:18 +0800

    modified code
    - made some changes

--- python_3/experiments/expr_tensorflow_basic_000.py
+++ python_3/experiments/expr_tensorflow_basic_000.py
@@ -7,11 +7,13 @@ Tensorflow-  simple example
 # install tensorflow on Windows 10 as pip install tensorflow --user
 import tensorflow as tf
 
+
 mnist = tf.keras.datasets.mnist
-
+# Load and prepare the MNIST dataset. 
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
+# Convert the samples from integers to floating-point numbers:
 x_train, x_test = x_train / 255.0, x_test / 255.0
-
+# Build the tf.keras.Sequential model by stacking layers. Choose an optimizer and loss function for training:
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(128, activation='relu'),
