6bf9200bfbff901367214372e57119cd6fa12a35
Ashish Dutt <ashish.dutt8@gmail.com>
2021-03-23 14:46:07 +0800

    modified script
    - add initial baseline model

--- r/experiments/expr_AV_predict_number_of_upvotes.R
+++ r/experiments/expr_AV_predict_number_of_upvotes.R
@@ -4,12 +4,30 @@
 # Problem type: regression
 # evaluation metric: RMSE
 
+# clean workspace
+rm(list = ls())
 # require libraries
-library(tidyverse)
+library(readr)
+library(rpart)
+library(rpart.plot)
 
 # read the data
-dat_train<- read_csv("data/av_train_NIR5Yl1.csv", na = c("","NA"))
-dat_test<- read_csv("data/av_test_8i3B3FC.csv", na = c("","NA"))
-colnames(dat_train)
-colnames(dat_test)
-table(dat_train$Upvotes)
+train_data<- read_csv("data/av_train_NIR5Yl1.csv", na = c("","NA"))
+test_data<- read_csv("data/av_test_8i3B3FC.csv", na = c("","NA"))
+colnames(train_data)
+table(train_data$Upvotes)
+
+# initial model 
+fit <- rpart(formula = Upvotes~., data = train_data, method = "anova", minbucket = 10, cp = -1)
+rpart.plot(fit)
+# Variable importance
+fit$variable.importance
+
+# Initial baseline prediction
+predicted <- predict(fit, test_data)
+summary(predicted)
+
+# create initial baseline submission file
+submission <- tibble('ID' = test_data$ID, 'Upvotes' = predicted)
+write_csv(submission, 'data/av_predictupvote_baseline_subms.csv')
+# Initial public leaderboard score with baseline model # 1854.556
