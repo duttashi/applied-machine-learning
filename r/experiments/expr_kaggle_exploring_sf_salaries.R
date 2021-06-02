008dcea012d7b2ed1269b652d0f80e835c83d3a8
Ashish Dutt <ashish.dutt8@gmail.com>
2021-06-02 10:40:38 +0800

    modified code
    - add new code: feature engineering to create new variables

--- r/experiments/expr_kaggle_exploring_sf_salaries.R
+++ r/experiments/expr_kaggle_exploring_sf_salaries.R
@@ -2,10 +2,11 @@
 # Possible hypothesis to check are;
 # 1. Salary depends on profession type
 
+rm(list = ls())
 
 # required libraries
 library(tidyverse)
-rm(list = ls())
+
 # Exploratory Data Analysis
 df<- read_csv("data/kaggle_sfsalaries.csv", na = c("", NA))
 dim(df) # 148654 rows 13 cols
@@ -14,16 +15,32 @@ colSums(is.na(df)) # vars benefits, status, notes are completely blank. remove t
 
 # drop all blank cols
 df$Id<- NULL
-df$Benefits<- NULL
 df$Notes<- NULL
-df$Status<- NULL
+df$Agency<- NULL # static with only 1 value
 
 # Feature Engineering
-job_legal_type<- 'police|fire|sheriff|attorney|lawyer|judge'
-job_health_type<- 'nursing|nurse|physician|anethist|forensic|doctor'
+job_type_legal<- 'police|fire|sheriff|attorney|lawyer|judge|auditor|inspector'
+job_type_health<- 'nursing|nurse|physician|anethist|forensic|doctor'
+job_type_manufc<- 'building|bricklayer|contract|architect|architecturalengineer|landscape|design|designer|technician'
+job_type_admn<- 'adm|admin|administrative|administrator|clerk|assessor'
+job_type_secur<- 'security|guard'
+job_type_edu<- 'instructor'
+job_type_animal<- 'animal|aquatics|aquatic'
+job_type_ojt<- 'apprentice|intern'
+# create new variables. Assign boolean value basis of other cols
 df<- df %>%
   # lowercase all character variables
   mutate(across(where(is.character), tolower))%>%
   # create new logical colum from jobTitle type col
-  mutate(job_lgl = str_detect(JobTitle, job_legal_type))%>%
-  mutate(job_hlth = str_detect(JobTitle, job_health_type))
+  mutate(job_lgl = str_detect(JobTitle, job_type_legal))%>%
+  mutate(job_hlth = str_detect(JobTitle, job_type_health))%>%
+  mutate(job_manuf = str_detect(JobTitle, job_type_manufc))%>%
+  mutate(job_admin = str_detect(JobTitle, job_type_admn))%>%
+  mutate(job_securty = str_detect(JobTitle, job_type_secur))%>%
+  mutate(job_edu = str_detect(JobTitle, job_type_edu))%>%
+  mutate(job_ojt = str_detect(JobTitle, job_type_ojt))
+
+table(df$job_lgl)
+table(df$job_admin)
+table(df$job_securty)
+table(df$job_ojt)
