b2493cf4face96b634b52c4aa63b4005f180a745
Ashish Dutt <ashish.dutt8@gmail.com>
2021-07-19 13:20:24 +0530

    modified code
    - add code for data visuals

--- python_3/experiments/expr_kaggle_books_visuals.py
+++ python_3/experiments/expr_kaggle_books_visuals.py
@@ -9,8 +9,19 @@ import pandas as pd
 import seaborn as sns
 
 # load data
-df = pd.read_csv("../../data/kaggle_bestseller_books.csv")
-print(df.head(5), df.columns, df.shape, df.dtypes)
+df = pd.read_csv("../../data/kaggle_bestseller_books.csv",
+                 dtype = {'Name':'category',
+                          'Author':'category',
+                          'Genre':'category'})
+print("data shape: ",df.shape,
+      df.head(5), "\n","Variable\n",df.columns,
+      "Variable data types:\n",df.dtypes)
+print("\n### Data summary ###")
+print("\nNo. of unique authors: ",df['Author'].nunique())
+print("\nGenre:\n ", df['Genre'].value_counts(normalize=True))
+print("\nUser Rating:\n ", df['User Rating'].describe())
+
+
 # print(df.Author.value_counts)
 
 # Seaborn: https://seaborn.pydata.org/tutorial/relational.html
@@ -19,15 +30,12 @@ sns.set(style="white", color_codes=True)
 
 # scatterplot: should be used when both variables are numeric
 sns.relplot(x="Reviews", y="Price", data=df)
-# sns.relplot(x="Year", y="Reviews", data=df)
-# sns.relplot(x="Year", y="Price", data=df)
 sns.relplot(x="User Rating", y="Price", data=df)
 sns.relplot(x="User Rating", y="Reviews", data=df)
 ## coloring the points according to a third variable. In seaborn, this is referred to as using a “hue semantic”
 # sns.relplot(x="Reviews", y="Price", hue="Genre", data=df)
 
 # lineplot
-
 ## lineplot with confidence plot
 sns.relplot(x="User Rating", y="Reviews", data=df, kind="line")
 ## lineplot without confidence plot
@@ -38,3 +46,9 @@ sns.relplot(x="User Rating", y="Reviews", data=df, ci="sd", kind="line");
 sns.relplot(x="User Rating", y="Reviews", data=df, estimator=None, ci="sd", kind="line");
 sns.relplot(x="User Rating", y="Reviews", hue = "Price",data=df, kind="line");
 
+# boxplot: for categorical and continuous vars
+sns.boxplot(x= 'Genre', y='Reviews' ,data = df)
+
+# histogram: 
+## Visualizing distributions of data
+sns.histplot("Price", data=df)
