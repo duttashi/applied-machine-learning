05e32baa8d72243a131e37c5aa489c2fda8ced9c
duttashi <ashish.dutt8@gmail.com>
2021-08-25 16:38:52 +0530

    create script
    - initial commit

---
+++ python_3/experiments/expr_kaggle_amstr_houseprice_pred.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 25 12:43:20 2021
+Data source: https://www.kaggle.com/thomasnibb/amsterdam-house-price-prediction
+@author: Ashish
+"""
+# import required libraries
+import pandas as pd
+import seaborn as sns
+
+# read data in global variable
+df = pd.read_csv('../../data/kaggle_amstr_houseprice.csv')
+
+def preprocess(data = df):
+    
+    # describe data
+    print("\n Data shape: ", df.shape)
+    print("\n Data types\n", df.dtypes)
+    print("\n missing vals: ", df.isnull().sum())
+    print("\n Mean of missing values:\n ", df.isnull().mean())
+    
+    # data management
+    
+    # lowercase all column names
+    df.columns = [x.lower() for x in df.columns]
+       
+    # filter out missing data greater than 80%
+    df_missing = df[df.isnull().values.any(axis=1)]
+    print("\n missing values: ", df_missing.isnull().sum())
+    clean_df = df.dropna()
+          
+    return clean_df
+
+def univariate_visuals(data = df):
+    sns.set(color_codes = True)
+    sns.displot(data=df, x='price', binwidth=3)
+        
+    return
+
+def bivariate_visuals(data = df):
+    sns.set(color_codes = True)
+    sns.jointplot(data=df, x='price', y='area')
+    # sns.pairplot(df)
+    return
+
+if __name__ == "__main__":
+    clean_df = preprocess(df)
+    print(type(clean_df))
+    print("\n clean dataframe",clean_df.isnull().mean())
+    univariate_visuals(clean_df)
+    bivariate_visuals(clean_df)
