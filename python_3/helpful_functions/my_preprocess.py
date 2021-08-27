534835ce58febaf89f4b3eb59d32d2a80e9a6d3b
duttashi <ashish.dutt8@gmail.com>
2021-08-27 16:41:17 +0530

    modified code
    - add custom function for missing data imputation
    - add custom function for rearranging data frame columns

--- python_3/helpful_functions/my_preprocess.py
+++ python_3/helpful_functions/my_preprocess.py
@@ -5,6 +5,7 @@ Created on Mon Sep 21 08:21:18 2020
 @author: Ashish
 """
 import pandas as pd
+import numpy as np
 # def percent_value_counts(df, feature):
 #     """This function takes in a dataframe and a column and finds the percentage of the value_counts"""
 #     percent = pd.DataFrame(round(df.loc[:,feature].value_counts(dropna=False, normalize=True)*100,2))
@@ -19,9 +20,37 @@ import pandas as pd
 def find_missing(df):
     # find variable with 90% missing data, filter them out
     # impute continuous with median and categorical with mode
+    total_missing = df.isnull().sum().sort_values(ascending=False)
     percent_missing = round( df.isnull().sum() * 100 / len(df), 3)
-    missing_value_df = pd.DataFrame({'column_name': df.columns,
-                                     'percent missing': percent_missing}
+    missing_value_df = pd.DataFrame({'Variable': df.columns,
+                                     'Total': total_missing,
+                                     'Percent missing': percent_missing}
                                     )
     
     return missing_value_df
+
+# rearrange dataframe by separating categorical & continuous variables
+def rearrange_dataframe(df):
+    # find all categorical cols
+    # print(df.dtypes)
+    catCols = [col for col in df.columns if df[col].dtypes == 'O']
+    numCols = [col for col in df.columns if df[col].dtypes == np.float64 or df[col].dtypes == np.int64]
+    # concatenate both lists
+    allCols = catCols + numCols
+    df_rearranged = df[allCols]
+    
+    return df_rearranged
+
+def impute_missing(df):
+    # function to impute missing data
+    # median imputation for continuous & mode imputation for categorical
+    # Input: data frame Output: imputed dataframe
+    # median impute for continuous variables
+    # print("\n missing data before imputation\n", df.isnull().sum())
+    numCols = [col for col in df.columns if df[col].dtypes == np.float64]
+    catCols = [col for col in df.columns if df[col].dtypes == 'O']
+    df[numCols] = lambda x: x.fillna(x.median())
+    df[catCols] = lambda x: x.fillna(x.mode())
+    
+    # return imputed dataframe
+    return df
