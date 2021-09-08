d0bec6fb7a915f8352d35e2ad032a3b0549a408e
duttashi <ashish.dutt8@gmail.com>
2021-09-08 16:15:48 +0530

    modified script
    - add function for printing the dataframe name
    - add function for finding missing values in dataframe
    - add function for replacing blanks with NAN
    - add function for imputing missing values with median and mode
    - clean dataframes written to disc

--- python_3/practice/kaggle_learndata_eda.py
+++ python_3/practice/kaggle_learndata_eda.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 data source: https://www.kaggle.com/c/learnplatform-covid19-impact-on-digital-learning/data
-refernce: https://www.kaggle.com/ruchi798/covid-19-impact-on-digital-learning-eda-w-b#Reading-data-files-%F0%9F%91%93
+reference: https://www.kaggle.com/ruchi798/covid-19-impact-on-digital-learning-eda-w-b#Reading-data-files-%F0%9F%91%93
 """
 import pandas as pd
-import matplotlib.pyplot as plt
+import glob
+import numpy as np
 # import glob
 # read data
 df_products = pd.read_csv("../../data/learnplatform-covid19-impact-on-digital-learning/products_info.csv")
@@ -12,32 +13,32 @@ print(df_products.shape)
 df_districts = pd.read_csv("../../data/learnplatform-covid19-impact-on-digital-learning/districts_info.csv")
 print(df_districts.shape)
 
-# reading multiple files from a folder into a list
-# path = '../../data/learnplatform-covid19-impact-on-digital-learning/engagement_data' 
-# all_files = glob.glob(path + "/*.csv")
+#reading multiple files from a folder into a list
+path = '../../data/learnplatform-covid19-impact-on-digital-learning/engagement_data' 
+all_files = glob.glob(path + "/*.csv")
 
-# li = []
+li = []
 
-# for filename in all_files:
-#     df = pd.read_csv(filename, index_col=None, header=0)
-#     district_id = filename.split("/")[4].split(".")[0]
-#     df["district_id"] = district_id
-#     li.append(df)
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=None, header=0)
+    district_id = filename.split("/")[4].split(".")[0]
+    df["district_id"] = district_id
+    li.append(df)
     
-# df_engagement = pd.concat(li)
-# df_engagement = df_engagement.reset_index(drop=True)
-# print(df_engagement.shape)
+df_engagement = pd.concat(li)
+df_engagement = df_engagement.reset_index(drop=True)
+print(df_engagement.shape)
 
-# # write to disk
-# path = '../../data/learnplatform-covid19-impact-on-digital-learning/'
-# df_engagement.to_csv(path+'engagement_combine.csv')
-# print(df_engagement.close())
+# write to disk
+path = '../../data/learnplatform-covid19-impact-on-digital-learning/'
+df_engagement.to_csv(path+'engagement_combine.csv')
+print(df_engagement.close())
 
 # read the engagement data from disc
 path = '../../data/learnplatform-covid19-impact-on-digital-learning/'
 df_engagement = pd.read_csv(path+'engagement_combine.csv')
 print(df_engagement.shape)
-
+print(df_engagement.dtypes)
 def get_df_name(df):
     name =[x for x in globals() if globals()[x] is df][0]
     return name
@@ -63,23 +64,26 @@ def find_null_columns(dataframe):
     
     return list_of_nullcolumns
 
-def missing_data_percentage(data):
-    
-    data_na = (data.isnull().sum() / len(data)) * 100
-    
-    data_na = data_na.drop(data_na[data_na == 0].index).sort_values(ascending=False)[:30]
-    
-    return data_na
+# replace blank with nan
+def replace_blanks(dataframe):
+    dataframe = dataframe.replace(r'^\s*$', np.nan, regex=True)
+    return dataframe
 
-def missing_data_plot(data_na):
-    x = data_na
-    fig = plt.figure(figsize=(8, 6))
-    plt.plot(x)
-    # use plt.show() when the function is not returning a value
-    # plt.show()
-    # use return plt if the function is returning a plot
-    return fig
-
+def fill_missing(dataframe):
+    df_imputed = replace_blanks(dataframe)
+    for col in df_imputed.columns:
+        if(df_imputed[col].dtype == 'object'):
+            df_imputed[col] = df_imputed[col].fillna(df_imputed[col].mode()[0])
+        elif (df_imputed[col].dtype == 'int64'):
+            # df_imputed[col] = df_imputed[col].astype(str)
+            df_imputed[col] = df_imputed[col].fillna(df_imputed[col].median())
+        elif (df_imputed[col].dtype == 'float64'):
+            # df_imputed[col] = df_imputed[col].astype(int)
+            df_imputed[col] = df_imputed[col].fillna(df_imputed[col].median())
+        else:
+            continue
+    # return imputed dataframe
+    return df_imputed
 
 missdata = find_null_columns(df_engagement)
 print("missing engagement data", missdata)
@@ -87,4 +91,24 @@ missdata = find_null_columns(df_districts)
 print("missing districts data", missdata)
 missdata = find_null_columns(df_products)
 print("missing products data", missdata)
+
+# Impute missing data
+df1 = replace_blanks(df_districts)
+df_clean = fill_missing(df1)
+# write clean data to disk
+df_clean.to_csv(path+'clean_district.csv', sep=",")
+print(df_clean.isnull().sum())
+
+df1 = replace_blanks(df_products)
+df_clean = fill_missing(df1)
+# write clean data to disk
+df_clean.to_csv(path+'clean_products.csv', sep=",")
+print(df_clean.isnull().sum())
+
+df1 = replace_blanks(df_engagement)
+df_clean = fill_missing(df1)
+# write clean data to disk
+df_clean.to_csv(path+'clean_engage.csv', sep=",")
+print(df_clean.isnull().sum())
+
 
