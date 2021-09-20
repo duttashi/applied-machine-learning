b2b35b8157459d17ede1c8c462f867257b497f2d
duttashi <ashish.dutt8@gmail.com>
2021-09-20 14:25:16 +0530

    initial commit

---
+++ python_3/experiments/test_incr.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 20 10:09:39 2021
+Test user defined function
+@author: Ashish
+
+"""
+import expr_incr
+
+def test_incr():
+    assert expr_incr.inc(3) == 3
+    
+
