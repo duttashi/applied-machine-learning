01a0941f8a8e9ea59e87e114b6e8c38f369fc79e
duttashi <ashish.dutt8@gmail.com>
2021-10-11 16:38:15 +0530

    modified script
    - add data cleaning code

--- r/experiments/expr_kaggle_indian_food_analysis.R
+++ r/experiments/expr_kaggle_indian_food_analysis.R
@@ -7,11 +7,22 @@ library(tidyverse)
 # read data
 df <- read_csv(file = "data/kaggle_indian_food.csv",
                na = c(-1,"NA"))
-colSums(is.na(df))
 glimpse(df)
 
-?geom_histogram
-# basic visuals
-ggplot(data = df)+
-  geom_bar(aes(x= diet), stat = "count")+
-  theme_bw()
+# create 10 cols
+cols2Add<- paste("ingre",1:10, sep = "_")
+df[, cols2Add]<- NA
+
+# separate ingredients into separate cols
+df<- df %>%
+  separate(ingredients, into = c("ingre_1","ingre_2","ingre_3",
+                                 "ingre_4","ingre_5","ingre_6",
+                                 "ingre_7","ingre_8","ingre_9",
+                                 "ingre_10"), 
+           sep = ",", extra = "drop", fill = "right")
+colSums(is.na(df))
+# drop cols
+names(df)
+df<- df[,-c(7:11)]
+colSums(is.na(df))
+glimpse(df)
