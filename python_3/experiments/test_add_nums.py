db767588336f1b178ef7ffd1bbe3fb794309ff76
duttashi <ashish.dutt8@gmail.com>
2021-10-13 11:23:36 +0530

    create script
    - user define function and testing module

---
+++ python_3/experiments/test_add_nums.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 12 15:34:54 2021
+Objective: test module for swap numbers in list
+@author: Ashish
+"""
+import expr_swap_list_nums
+
+def test_add_nums():
+    assert expr_swap_list_nums.add_nums(5) == 7
+
